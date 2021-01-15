2954215e5692e7cb37c26f1cbf47c029194ed728
khoa110298 <nguyenkhoa15011998@gmail.com>
2021-01-15 14:46:53 +0700

    upbai

--- bai11_web_service_&_RESTfull/thuc_hanh/customer_RESTfull/src/main/java/vn/codegym/controller/CustomerController.java
+++ bai11_web_service_&_RESTfull/thuc_hanh/customer_RESTfull/src/main/java/vn/codegym/controller/CustomerController.java
@@ -1,12 +1,12 @@
 package vn.codegym.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.util.UriComponentsBuilder;
 import vn.codegym.entity.Customer;
 import vn.codegym.service.CustomerService;
 
@@ -32,5 +32,34 @@ public class CustomerController {
             return new ResponseEntity<>(HttpStatus.NOT_FOUND);
         }
         return new ResponseEntity<>(HttpStatus.OK);
+    }
+    @PostMapping("/api/customer")
+    public ResponseEntity<Void> createCustomer(@RequestBody Customer customer, UriComponentsBuilder uriComponentsBuilder){
+        customerService.save(customer);
+        HttpHeaders headers = new HttpHeaders();
+        headers.setLocation(uriComponentsBuilder.path("/api/customer/{id}").buildAndExpand(customer.getId()).toUri());
+        return new ResponseEntity<>(headers,HttpStatus.CREATED);
+    }
+    @PutMapping("/api/customer/{id}")
+    public ResponseEntity<Customer> updateCustomer(@PathVariable("id") long id,@RequestBody Customer customer){
+        Customer cus = customerService.findById(id);
+        if (cus == null){
+            return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+        }
+        cus.setFirstName(customer.getFirstName());
+        cus.setLastName(customer.getLastName());
+        cus.setId(customer.getId());
+        customerService.save(cus);
+
+        return new ResponseEntity<>(cus,HttpStatus.OK);
+    }
+    @DeleteMapping("/api/customer/{id}")
+    public ResponseEntity<Void> deleteCustomer(@PathVariable("id") long id){
+        Customer customer = customerService.findById(id);
+        if (customer == null){
+            return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+        }
+        customerService.remove(id);
+        return new ResponseEntity<>(HttpStatus.NO_CONTENT);
     }
 }
